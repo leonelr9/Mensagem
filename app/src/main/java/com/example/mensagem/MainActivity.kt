567129e26b5dd7be4edcd75571f0712f98128916
Leonel Rita <leonelrita@hotmail.com>
2021-04-21 11:56:36 +0100

    Criar a função para enviar a mensagem Obter a mensagem introduzida pelo utilizador

--- app/src/main/java/com/example/mensagem/MainActivity.kt
+++ app/src/main/java/com/example/mensagem/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.mensagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun enviaMensagem(View: View){
+        val editTextMensagem = findViewById<EditText>(R.id.editTextMensagem)
+        val mensagem = editTextMensagem.text.toString()
     }
 }
