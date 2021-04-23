5ce119474331babec5333c4fc6a263e663cee2d5
Leonel Rita <leonelrita@hotmail.com>
2021-04-23 11:31:57 +0100

    Utilizar constantes (nome) para passar a mensagem para a outra tela. Usado para não haver erros de escrita, e caso hajo seja detetado quando o programa é compilado

--- app/src/main/java/com/example/mensagem/MainActivity.kt
+++ app/src/main/java/com/example/mensagem/MainActivity.kt
@@ -11,12 +11,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
     fun enviaMensagem(View: View){
         val editTextMensagem = findViewById<EditText>(R.id.editTextMensagem)
         val mensagem = editTextMensagem.text.toString()
 
         val intent = Intent(this, MostraMensagemActivity::class.java)
-        intent.putExtra("MENSAGEM", mensagem)
+        intent.putExtra(INFO_EXTRA_MENSAGEM, mensagem)
         startActivity(intent)
+    }
+
+    companion object {
+        const val INFO_EXTRA_MENSAGEM = "MENSAGEM"
     }
 }
